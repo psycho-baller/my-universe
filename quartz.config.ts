eb1b0060a676828c7f6bf54c047ce42c283c3528
Rami Maalouf <rami.rami@ucalgary.ca>
2025-04-17 11:16:05 -0600

    fix build?

--- quartz.config.ts
+++ quartz.config.ts
@@ -110,7 +110,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
